5ead2638738bbc2e9368dd819389c89d003bd85b
DueWesternersProgramming <72952036+DueWesternersProgramming@users.noreply.github.com>
2021-03-01 18:30:48 -0500

    More Slight Changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
+import frc.robot.commands.Autonomous.*;
 import frc.robot.subsystems.IntakeSubsystem;
 
 /**

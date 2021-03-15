e379f2a20af2492d8552c279258c2cb7bcf8d4f0
Due Westerners <frc-programmers@duewestrobotics.org>
2021-03-15 18:38:19 -0500

    more small changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
 import frc.robot.commands.Autonomous.AutoNav.*;
 import frc.robot.commands.Autonomous.GalacticSearch.*;
+import frc.robot.commands.Autonomous.*;
 import frc.robot.subsystems.IntakeSubsystem;
 
 /**

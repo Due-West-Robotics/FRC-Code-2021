f816905066d5d66d1eb8cad5155739beab251943
DueWesternersProgramming <72952036+DueWesternersProgramming@users.noreply.github.com>
2021-03-20 18:38:49 -0400

    Finished PathBRed

--- src/main/java/frc/robot/commands/Autonomous/GalacticSearch/PathBRed.java
+++ src/main/java/frc/robot/commands/Autonomous/GalacticSearch/PathBRed.java
@@ -14,7 +14,19 @@ public class PathBRed extends SequentialCommandGroup {
         m_drive = driveSubsystem;
         addCommands(
             new DriveDistance(m_drive,-24, 0.65),
-            new StartIntake(m_intake)
+            new StartIntake(m_intake),
+            new DriveDistance(m_drive,34, 0.5),
+            new TurnDegrees(m_drive, 90, 0.5, DriveConstants.kLeft, 19.25),
+            new DriveDistance(m_drive,32.372, 0.5),
+            new TurnDegrees(m_drive, 90, 0.5, DriveConstants.kRight, 17.391),
+            new DriveDistance(m_drive,25.218, 0.5),
+            new TurnDegrees(m_drive, 90, 0.5, DriveConstants.kRight, 17.391),
+            new DriveDistance(m_drive,81.622, 0.5),
+            new TurnDegrees(m_drive, 180, 0.5, DriveConstants.kLeft, 30),
+            new DriveDistance(m_drive,60, 0.5),
+            new TurnDegrees(m_drive, 90, 0.5, DriveConstants.kRight, 21.622),
+            new DriveDistance(m_drive,105, 0.75),
+            new StopIntake(m_intake);
         );
     }
 }
